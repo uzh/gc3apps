e706cc8b7c72e08825a8514ab6839b5c24490755
Sergio Maffioletti <sergio.maffioletti@uzh.ch>
2019-05-31 12:09:45 +0200

    sergio: bugfix

--- gkovesh/gkovesh.py
+++ gkovesh/gkovesh.py
@@ -191,7 +191,7 @@ class GkoveshScript(SessionBasedScript):
         assert self.group_repetitions > 0, "repetitions sould be higher than groups"
 
         self.data_group_size = max(self.params.chunk,
-                                   _get_data_group_size(self.params.input_folder)
+                                   _get_data_group_size(self.params.input_folder))
         self.log.info("Setting data group size to {0}bytes".format(self.data_group_size))
 
     def new_tasks(self, extra):
